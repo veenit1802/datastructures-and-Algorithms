f40b415d74420ca8382964c0ded33c419585f810
veenit1802 <veenitshukla20@gmail.com>
2020-09-11 13:49:03 +0530

    first commit

--- dynamicprogramming/lonreasubs.cpp
+++ dynamicprogramming/lonreasubs.cpp
@@ -1,19 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int LRSLength(string x,int m,int n)
+int LRSLength(string x,int m,int n,auto &lookup)
 {
 	if(m==0 || n==0)
 		return 0;
-		
-	if(x[m-1]==x[n-1] and m!=n)
-		return LRSLength(x,m-1,n-1)+1;
-	return max( LRSLength(x,m-1,n),LRSLength(x,m,n-1) );
+	string key=to_string(m)+"|"+to_string(n);
+	if(lookup.find(key)==lookup.end())
+	{
+		if(x[m-1]==x[n-1] and m!=n)
+			lookup[key]= LRSLength(x,m-1,n-1,lookup)+1;
+		else
+			lookup[key]= max( LRSLength(x,m-1,n,lookup),LRSLength(x,m,n-1,lookup) );
+	}
+
+	return lookup[key];
 }
 
 int main()
 {
 	string x="ATACTCGGA";
 	int m=x.length();
-	cout<<LRSLength(x,m,m);
+	unordered_map<string,int>lookup;
+	cout<<LRSLength(x,m,m,lookup);
 }
