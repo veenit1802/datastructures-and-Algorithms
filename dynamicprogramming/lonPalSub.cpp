286ad010b4f816bbb7aa86630c81c3571f55f671
veenit1802 <veenitshukla20@gmail.com>
2020-09-11 13:18:38 +0530

    first commit

--- dynamicprogramming/lonPalSub.cpp
+++ dynamicprogramming/lonPalSub.cpp
@@ -1,23 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int longestPalindrome(string x,int i,int j)
+int longestPalindrome(string x,int i,int j,auto &lookup)
 {
 	if(i>j)
 		return 0;
 	if(i==j)
 		return 1;
-	if(x[i]==x[j])
-		return longestPalindrome(x,i+1,j-1)+2;
-	return max( longestPalindrome(x,i+1,j),longestPalindrome(x,i,j-1) );
+	string key=to_string(i)+"|"+to_string(j);
+	if(lookup.find(key)==lookup.end())
+	{
+		if(x[i]==x[j])
+		lookup[key]= longestPalindrome(x,i+1,j-1,lookup)+2;
+		else
+		lookup[key]= max( longestPalindrome(x,i+1,j,lookup),longestPalindrome(x,i,j-1,lookup) );
+	}
+	return lookup[key];
 }
 
 int main()
 {
 	string x="ABBDCACB";
 	int n=x.length();
-
-	cout<<longestPalindrome(x,0,n-1);
+	unordered_map<string,int>lookup;
+	cout<<longestPalindrome(x,0,n-1,lookup);
 
 
 	return 0;
