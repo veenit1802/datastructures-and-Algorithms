aea0875981092df4e520e324261add1dfae72cce
veenit1802 <veenitshukla20@gmail.com>
2021-02-16 11:17:11 +0530

    dp

--- dynamicprogramming/Russian_Doll_Envelopes.cpp
+++ dynamicprogramming/Russian_Doll_Envelopes.cpp
@@ -1,12 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool cmp(const pair<int,int>&a,const pair<int,int>&b)
+{
+    if(a.first==b.first)
+    {
+        return a.second<b.second;
+    }
+    return a.first<b.first;
+}
 
 int main()
 {
-    int n;
+    int n,i;
     cin>>n;
-    
+
+    vector<pair<int,int>>v;
+    int l,r;
+    for(i=0;i<n;i++)
+    {
+        cin>>l>>r;
+        v.push_back({l,r});
+    }
+
+    sort(v.begin(),v.end(),cmp);
 
 
     return 0;
